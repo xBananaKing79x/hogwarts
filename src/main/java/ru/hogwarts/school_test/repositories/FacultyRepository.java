3778727492c3df151ac7c352960db89a27faab51
xBananaKing79x <khakimov.t@gmail.com>
2025-07-27 21:34:48 +0300

    delete classes

--- src/main/java/ru/hogwarts/school_test/repositories/FacultyRepository.java
+++
@@ -1,6 +0,0 @@
-package ru.hogwarts.school_test.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacultyRepository extends JpaRepository {
-}
--- src/main/java/ru/hogwarts/school_test/repositories/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package ru.hogwarts.school_test.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.hogwarts.school_test.model.Student;
-
-public interface StudentRepository extends JpaRepository <Student, Long> {
-}
